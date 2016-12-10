808e1daa409dc7fba1b414f2989dc0f14793ddf6
Alexander <alexsmirnoff2332@gmail.com>
2016-12-10 17:05:25 +0400

    Database Interconnection

--- BasketballStatistics.Data/Repository.cs
+++ BasketballStatistics.Data/Repository.cs
@@ -28,12 +28,12 @@ namespace BasketballStatistics.Data
         //    var result = teams.First();
         //    return result;
         //}
-        //public IQueryable<string> TeamBoxLoadWithData(Context context)
-        //{
-        //    var result = from team in context.Teams
-        //                 select team.Name;
-        //    return result;
-        //}
+        public IQueryable<string> TeamBoxLoadWithData(Context context)
+        {
+            var result = from team in context.Teams
+                         select team.Name;
+            return result;
+        }
         public Match FindMatch(Context context, string firstTeam, string secondTeam, string finalScore)
         {
             var search = from match in context.Matches
@@ -43,43 +43,55 @@ namespace BasketballStatistics.Data
             return result;
         }
 
-        //public IQueryable<CommandStatisticsViewModel> GameStatisticsOfTeam(Context context, Match match, string nameOfTeam)
-        //{
-        //    var result = from stat in context.Statistics
-        //                 where (stat.Match == match) && (stat.Match.Team1.Name == nameOfTeam || stat.Match.Team2.Name == nameOfTeam)
-        //                 select new CommandStatisticsViewModel
-        //                 {
-        //                     Points = stat.Points,
-        //                     Assists = stat.Assists,
-        //                     Rebounds = stat.Rebounds,
-        //                     Steals = stat.Steals,
-        //                     BlockedShots = stat.BlockedShots,
-        //                     ShotsFromGame = stat.ShotsFromGame,
-        //                     ShotsFromGameSuccessfull = stat.ShotsFromGameSuccessfull,
-        //                     ShotsFromGamePercent = (((double)stat.ShotsFromGameSuccessfull / stat.ShotsFromGame) * 100).ToString("#.##") + "%",
-        //                     ShotsFromGameFar = stat.ShotsFromGameFar,
-        //                     ShotsFromGameFarSuccessfull = stat.ShotsFromGameFarSuccessfull,
-        //                     ShotsFromGameFarPercent = (((double)stat.ShotsFromGameFarSuccessfull / stat.ShotsFromGameFar) * 100).ToString("#.##") + "%"
-        //                 };
-        //    return result;
-        //}
+        public CommandStatisticsViewModel GameStatisticsOfTeam(Context context, Match match, string nameOfTeam)
+        {
+            var result = from stat in context.CommandStatistics
+                         where (stat.Match == match) && (stat.Match.Team1.Name == nameOfTeam || stat.Match.Team2.Name == nameOfTeam)
+                         select new CommandStatisticsViewModel
+                         {
+                             Points = stat.Points,
+                             Assists = stat.Assists,
+                             Rebounds = stat.Rebounds,
+                             Steals = stat.Steals,
+                             BlockedShots = stat.BlockedShots,
+                             ShotsFromGame = stat.ShotsFromGame,
+                             ShotsFromGameSuccessfull = stat.ShotsFromGameSuccessfull,
+                             ShotsFromGamePercent = (((double)stat.ShotsFromGameSuccessfull / stat.ShotsFromGame) * 100).ToString("#.##") + "%",
+                             ShotsFromGameFar = stat.ShotsFromGameFar,
+                             ShotsFromGameFarSuccessfull = stat.ShotsFromGameFarSuccessfull,
+                             ShotsFromGameFarPercent = (((double)stat.ShotsFromGameFarSuccessfull / stat.ShotsFromGameFar) * 100).ToString("#.##") + "%"
+                         };
+            return result.First();
+        }
 
-        //public void ShortStatisticsOfTeams(Context context)
-        //{
-        //    var teams = from team in context.Teams
-        //                select team;
-        //    foreach (var team in teams)
-        //    {
-        //        var wins = 0;
-        //        var loses = 0;
-        //        var statistics = from stat in context.CommandStatistics
-        //                         where stat.Team == team
-        //                         select stat;
-        //        foreach (var stats in statistics)
-        //        {
-        //            if 
-        //        }
-        //    }
-        //}
+        public List<ShortCommandStatisticsViewModel> ShortStatisticsOfTeams(Context context)
+        {
+            List<ShortCommandStatisticsViewModel> result = new List<ShortCommandStatisticsViewModel>();
+            var teams = from team in context.Teams
+                        select team;
+            foreach (var team in teams)
+            {
+                var wins = 0;
+                var loses = 0;
+                var statistics = from stat in context.CommandStatistics
+                                 where stat.Team == team
+                                 select stat;
+                foreach (var stats in statistics)
+                {
+                    if (stats.Match.Team1 == team)
+                    {
+                        if (stats.Match.Team1Score > stats.Match.Team2Score) wins++;
+                        if (stats.Match.Team1Score < stats.Match.Team2Score) loses++;
+                    }else
+                        if (stats.Match.Team2 == team)
+                    {
+                        if (stats.Match.Team1Score > stats.Match.Team2Score) loses++;
+                        if (stats.Match.Team1Score < stats.Match.Team2Score) wins++;
+                    }
+                }
+                result.Add(new ShortCommandStatisticsViewModel { Name = team.Name, Wins = wins, Loses = loses });
+            }
+            return result;
+        }
     }
 }
