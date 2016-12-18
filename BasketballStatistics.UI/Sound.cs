506027229beef7bd6f0996b4eb82ec82fa34e757
nekiy8 <nekiy23da@gmail.com>
2016-12-18 18:58:45 +0300

    Added sound and bug fixes in NewGameWindow

---
+++ BasketballStatistics.UI/Sound.cs
@@ -0,0 +1,13 @@
+﻿using System.Media;
+
+namespace BasketballStatistics.UI
+{
+    static class Sound
+    {
+        public static void TimeIsOver()
+        {
+            SystemSounds.Asterisk.Play();
+        }
+    }
+    
+}
